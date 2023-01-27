f08b4c304feba3c7c034215ac50688e94f0a70b9
Vasil Sokolov <vasil@afterlogic.com>
2023-01-27 12:45:54 +0200

    Added CS Fixer check to pre-commit

--- Classes/Connector.php
+++ Classes/Connector.php
@@ -17,93 +17,82 @@ namespace Aurora\Modules\DropboxAuthWebclient\Classes;
  */
 class Connector extends \Aurora\Modules\OAuthIntegratorWebclient\Classes\Connector
 {
-	protected $Name = 'dropbox';
+    protected $Name = 'dropbox';
 
-	public function CreateClient($sId, $sSecret, $sScope)
-	{
-		if (empty($sId) || empty($sSecret))
-		{
-			throw new \Aurora\Modules\OAuthIntegratorWebclient\Exceptions\NotConfigured(
-				\Aurora\Modules\OAuthIntegratorWebclient\Enums\ErrorCodes::NotConfigured
-			);
-		}
+    public function CreateClient($sId, $sSecret, $sScope)
+    {
+        if (empty($sId) || empty($sSecret)) {
+            throw new \Aurora\Modules\OAuthIntegratorWebclient\Exceptions\NotConfigured(
+                \Aurora\Modules\OAuthIntegratorWebclient\Enums\ErrorCodes::NotConfigured
+            );
+        }
 
-		$sRedirectUrl = \rtrim(\MailSo\Base\Http::SingletonInstance()->GetFullUrl(), '\\/ ').'/?oauth=' . $this->Name;
-		if (!\strpos($sRedirectUrl, '://localhost'))
-		{
-			$sRedirectUrl = \str_replace('http:', 'https:', $sRedirectUrl);
-		}
+        $sRedirectUrl = \rtrim(\MailSo\Base\Http::SingletonInstance()->GetFullUrl(), '\\/ ').'/?oauth=' . $this->Name;
+        if (!\strpos($sRedirectUrl, '://localhost')) {
+            $sRedirectUrl = \str_replace('http:', 'https:', $sRedirectUrl);
+        }
 
-		$oClient = new \oauth_client_class;
-		$oClient->debug = true;#self::$Debug;
-		$oClient->debug_http = true;#self::$Debug;
-		$oClient->server = 'Dropbox2v2';
-		$oClient->redirect_uri = $sRedirectUrl;
-		$oClient->client_id = $sId;
-		$oClient->client_secret = $sSecret;
-		$oOAuthIntegratorWebclientModule = \Aurora\System\Api::GetModule('OAuthIntegratorWebclient');
-		if ($oOAuthIntegratorWebclientModule)
-		{
-			$oClient->configuration_file = $oOAuthIntegratorWebclientModule->GetPath() .'/Classes/OAuthClient/'.$oClient->configuration_file;
-		}
+        $oClient = new \oauth_client_class();
+        $oClient->debug = true;#self::$Debug;
+        $oClient->debug_http = true;#self::$Debug;
+        $oClient->server = 'Dropbox2v2';
+        $oClient->redirect_uri = $sRedirectUrl;
+        $oClient->client_id = $sId;
+        $oClient->client_secret = $sSecret;
+        $oOAuthIntegratorWebclientModule = \Aurora\System\Api::GetModule('OAuthIntegratorWebclient');
+        if ($oOAuthIntegratorWebclientModule) {
+            $oClient->configuration_file = $oOAuthIntegratorWebclientModule->GetPath() .'/Classes/OAuthClient/'.$oClient->configuration_file;
+        }
 
-		return $oClient;
-	}
+        return $oClient;
+    }
 
-	public function Init($sId, $sSecret, $sScope = '')
-	{
-		$mResult = false;
+    public function Init($sId, $sSecret, $sScope = '')
+    {
+        $mResult = false;
 
-		$oClient = $this->CreateClient($sId, $sSecret, $sScope);
+        $oClient = $this->CreateClient($sId, $sSecret, $sScope);
 
-		if($oClient)
-		{
-			$oUser = null;
-			if(($success = $oClient->Initialize()))
-			{
-				if(($success = $oClient->Process()))
-				{
-					if(\strlen($oClient->access_token))
-					{
-						$success = $oClient->CallAPI(
-							'https://api.dropbox.com/2/users/get_current_account',
-							'POST',
-							null,
-							array(
-								'FailOnAccessError' => true,
-								'RequestContentType' => 'application/json'
-							),
-							$oUser
-						);
-					}
-				}
-				$success = $oClient->Finalize($success);
-			}
+        if ($oClient) {
+            $oUser = null;
+            if (($success = $oClient->Initialize())) {
+                if (($success = $oClient->Process())) {
+                    if (\strlen($oClient->access_token)) {
+                        $success = $oClient->CallAPI(
+                            'https://api.dropbox.com/2/users/get_current_account',
+                            'POST',
+                            null,
+                            array(
+                                'FailOnAccessError' => true,
+                                'RequestContentType' => 'application/json'
+                            ),
+                            $oUser
+                        );
+                    }
+                }
+                $success = $oClient->Finalize($success);
+            }
 
-			if($oClient->exit)
-			{
-				exit;
-			}
+            if ($oClient->exit) {
+                exit;
+            }
 
-			if($success && $oUser)
-			{
-				$mResult = array(
-					'type' => $this->Name,
-					'id' => $oUser->account_id,
-					'name' => $oUser->name->display_name,
-					'email' => isset($oUser->email) ? $oUser->email : '',
-					'access_token' => $oClient->access_token,
-					'scopes' => \explode('|', $sScope)
+            if ($success && $oUser) {
+                $mResult = array(
+                    'type' => $this->Name,
+                    'id' => $oUser->account_id,
+                    'name' => $oUser->name->display_name,
+                    'email' => isset($oUser->email) ? $oUser->email : '',
+                    'access_token' => $oClient->access_token,
+                    'scopes' => \explode('|', $sScope)
 
-				);
-			}
-			else
-			{
-				$mResult = false;
-				$oClient->ResetAccessToken();
-			}
-		}
+                );
+            } else {
+                $mResult = false;
+                $oClient->ResetAccessToken();
+            }
+        }
 
-		return $mResult;
-	}
+        return $mResult;
+    }
 }
